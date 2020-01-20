af59170e11bb14a9e5bde65ba8d474c499643fde
Elena Kashtanova <lenachestnut@gmail.com>
2020-01-20 22:14:19 +0300

    Add logic for creating an initial grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.grid-container');
+let squareNumber = 16;
+
+for (let i = 0; i < squareNumber; i++) {
+
+    let columns = `repeat(${i + 1}, 1fr)`;
+    container.style.gridTemplateColumns = columns;
+
+    let rows = `repeat(${i + 1}, 1fr)`;
+    container.style.gridTemplateRows = rows;
+
+    for (let j = 0; j < squareNumber; j++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('square');
+        container.appendChild(gridSquare);
+    }
+
+}
